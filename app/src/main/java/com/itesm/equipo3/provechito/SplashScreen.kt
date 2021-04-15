5d6616b08dc77276c38d1858712ee715efef100b
Dpalme <diegoPalmerin24@gmail.com>
2021-04-15 02:20:54 -0500

    colors and splash screen

---
+++ app/src/main/java/com/itesm/equipo3/provechito/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.itesm.equipo3.provechito
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+// Autor: Diego Pb
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val timer = object: CountDownTimer(1000, 50) {
+            override fun onTick(millisUntilFinished: Long) {
+                return
+            }
+
+            override fun onFinish() {
+                val i = Intent(baseContext, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+        }
+        timer.start()
+
+    }
+}
