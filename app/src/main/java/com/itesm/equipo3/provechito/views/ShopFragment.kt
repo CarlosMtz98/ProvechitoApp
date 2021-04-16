8838715596eb4d7702728c6127d8913fb74757b0
Jajavivargas <42326546+A01377718@users.noreply.github.com>
2021-04-16 02:33:31 -0500

    Commmit Dimens

--- app/src/main/java/com/itesm/equipo3/provechito/views/ShopFragment.kt
+++ app/src/main/java/com/itesm/equipo3/provechito/views/ShopFragment.kt
@@ -1,21 +1,35 @@
 package com.itesm.equipo3.provechito.views
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import com.itesm.equipo3.provechito.databinding.FragmentShopBinding
 import com.itesm.equipo3.provechito.models.Ingredient
 import java.util.*
 import kotlin.collections.ArrayList
 
-class ShopFragment : Fragment(), ClickListener {
+class ShopFragment : Fragment(), ClickListener, CustomListeners {
     private lateinit var arrIngredients: ArrayList<Ingredient>
     private var _binding: FragmentShopBinding? = null
     private val binding get() = _binding!!
+
+    companion object {
+        private val TAG : String = ShopFragment::class.java.getSimpleName()
+        fun newIntent(context : Context) : Intent {
+            val intent : Intent = Intent(context, ShopFragment::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            return intent
+        }
+    }
+
+    private lateinit var adapter : CustomAdapter
+    private lateinit var itemList : MutableList<CustomViewModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,8 +68,20 @@ class ShopFragment : Fragment(), ClickListener {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+    }
+
     override fun clicked(posicion: Int) {
         val ingredient = arrIngredients[posicion]
         println("posicion: ${ingredient}")
+    }
+
+    override fun onClickLeft(item: CustomViewModel, position: Int) {
+        println("Pico a la izquierda $position")
+    }
+
+    override fun onClickRight(item: CustomViewModel, position: Int) {
+        println("Pico a la derecha $position")
     }
 }
