bda61ff1bdd8feac7039308b803780cf2731130f
CarlosMtz98 <cmtz.rdz.98@gmail.com>
2021-04-15 21:29:38 -0500

    - Resolve conflicts of dev

--- app/src/main/java/com/itesm/equipo3/provechito/SplashScreen.kt
+++ app/src/main/java/com/itesm/equipo3/provechito/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import com.itesm.equipo3.provechito.views.MainActivity
 
 // Autor: Diego Pb
 
