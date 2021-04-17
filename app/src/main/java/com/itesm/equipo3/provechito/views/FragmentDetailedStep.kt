6b66b11379634069fa346c3f72e908c24d3db3b3
Jajavivargas <42326546+A01377718@users.noreply.github.com>
2021-04-16 19:09:35 -0500

    Navigation Review

--- app/src/main/java/com/itesm/equipo3/provechito/views/FragmentDetailedStep.kt
+++ app/src/main/java/com/itesm/equipo3/provechito/views/FragmentDetailedStep.kt
@@ -1,5 +1,6 @@
 package com.itesm.equipo3.provechito.views
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,9 +11,19 @@ import com.itesm.equipo3.provechito.databinding.FragmentDetailedStepBinding
 class FragmentDetailedStep : Fragment() {
     private var _binding: FragmentDetailedStepBinding? =  null
     private val binding get() = _binding!!
+    private lateinit var listener: HomeClcikListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is HomeClcikListener) {
+            listener = context
+        } else {
+            throw ClassCastException(context.toString() + " must implement HomeClickListner.")
+        }
     }
 
     override fun onCreateView(
@@ -21,6 +32,10 @@ class FragmentDetailedStep : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDetailedStepBinding.inflate(layoutInflater, container, false)
+        binding.btnNextStep.setOnClickListener {
+            val reviewFragment = ReviewFragment()
+            listener.onNextClicked()
+        }
         return binding.root
     }
 
