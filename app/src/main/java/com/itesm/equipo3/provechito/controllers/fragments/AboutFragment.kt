06def917f39d2aaf934eafb8174afb74fe24956d
ZoeCD <>
2021-06-01 22:22:16 -0500

    Botón about

--- app/src/main/java/com/itesm/equipo3/provechito/controllers/fragments/AboutFragment.kt
+++ app/src/main/java/com/itesm/equipo3/provechito/controllers/fragments/AboutFragment.kt
@@ -1,23 +1,31 @@
 package com.itesm.equipo3.provechito.controllers.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.itesm.equipo3.provechito.R
+import com.itesm.equipo3.provechito.controllers.listeners.SettingsClickListener
+import com.itesm.equipo3.provechito.databinding.FragmentAboutBinding
+import com.itesm.equipo3.provechito.databinding.FragmentSettingsBinding
 
 class AboutFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance(): AboutFragment {
+            return AboutFragment()
+        }
     }
 }
