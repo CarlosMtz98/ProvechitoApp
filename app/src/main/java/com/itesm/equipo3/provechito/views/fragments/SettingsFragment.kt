f8b18c3f13778f20eb40e47075c847ec499bb6ba
Carlos Martínez <cmtz.rdz.98@gmail.com>
2021-06-04 17:56:25 -0500

    Logout implemented

--- app/src/main/java/com/itesm/equipo3/provechito/views/fragments/SettingsFragment.kt
+++ app/src/main/java/com/itesm/equipo3/provechito/views/fragments/SettingsFragment.kt
@@ -2,19 +2,24 @@ package com.itesm.equipo3.provechito.views.fragments
 
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.itesm.equipo3.provechito.views.listeners.HomeClickListener
+import com.itesm.equipo3.provechito.R
 import com.itesm.equipo3.provechito.databinding.FragmentSettingsBinding
+import com.itesm.equipo3.provechito.views.activities.OnboardingActivity
+import com.itesm.equipo3.provechito.views.listeners.HomeClickListener
 
 
 class SettingsFragment : Fragment(){
     private lateinit var listener: HomeClickListener
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+    private var preferences: SharedPreferences? = null
 
 
     override fun onCreateView(
@@ -24,6 +29,21 @@ class SettingsFragment : Fragment(){
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         binding.btnAbout.setOnClickListener {
             listener.onAboutClicked()
+        }
+
+        binding.btnLogout.setOnClickListener {
+            context?.let {
+                var preferences: SharedPreferences = it.getSharedPreferences(it.getString(R.string.app_name), Context.MODE_PRIVATE)
+                preferences.edit().clear().apply();
+                val i = Intent(it, OnboardingActivity::class.java)
+                val fm = this.activity?.supportFragmentManager
+                if (fm != null) {
+                    for (i in 0 until fm.backStackEntryCount) {
+                        fm.popBackStack()
+                    }
+                }
+                startActivity(i)
+            }
         }
         return binding.root
 
